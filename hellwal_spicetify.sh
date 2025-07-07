b1ba81cc3c6df3cbbd85f14f79d0ddecda7ca138
itmoth <itmoth4@gmail.com>
2025-07-06 19:42:41 -0700

    Update hellwal_spicetify.sh

--- hellwal_spicetify.sh
+++ hellwal_spicetify.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env sh
 
-hellwal_color="/home/itmoth/.cache/hellwal/color.ini"
+hellwal_color="~/.cache/hellwal/color.ini"
 cp $hellwal_color ~/.config/spicetify/Themes/Comfy/
 
 spicetify apply
